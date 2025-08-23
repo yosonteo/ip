dc69c88e6d9b9e743ef9d6a2207df30e2a94553d
yosonteo <teoyoson@gmail.com>
2025-08-23 21:38:30 +0800

    Update Jone.java

---
+++ src/main/java/Jone.java
@@ -0,0 +1,8 @@
+public class Jone {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Jone! \n"
+                + "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greet + "\n" + exit);
+    }
+}
