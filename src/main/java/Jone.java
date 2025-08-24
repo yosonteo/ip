bc6d5e81767c441261a5260c1047e338bd32ac08
yosonteo <teoyoson@gmail.com>
2025-08-24 17:49:31 +0800

    Update Jone.java

--- src/main/java/Jone.java
+++ src/main/java/Jone.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Jone {
     public static void main(String[] args) {
+        int taskCount = 0;
+        String[] tasks = new String[100];
         Scanner sc = new Scanner(System.in);
         String greet = "Hello! I'm Jone! \n"
-                + "What can I do for you?";
+                     + "What can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
 
         System.out.println(greet);
@@ -15,9 +17,15 @@ public class Jone {
             if (input.equals("bye")) {
                 System.out.println("      " + exit);
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("      " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("      added: " + input);
+                }
             }
-
-            System.out.println("      " + input);
-        }
     }
 }
