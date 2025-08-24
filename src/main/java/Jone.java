934e4ade66b52e8fb709af35215bf4696d648201
yosonteo <teoyoson@gmail.com>
2025-08-24 16:51:32 +0800

    Update Jone.java

--- src/main/java/Jone.java
+++ src/main/java/Jone.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Jone {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String greet = "Hello! I'm Jone! \n"
                 + "What can I do for you?";
         String exit = "Bye. Hope to see you again soon!";
-        System.out.println(greet + "\n" + exit);
+
+        System.out.println(greet);
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("      " + exit);
+                break;
+            }
+
+            System.out.println("      " + input);
+        }
     }
 }
